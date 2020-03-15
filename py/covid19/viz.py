6045e9b8b56154a8cc34292e3c71e68d19580249
guilherme <guilherme.dinisjunior@gmail.com>
2020-03-15 23:04:35 +0100

    Updates tooltip for info on charts

--- py/covid19/viz.py
+++ py/covid19/viz.py
@@ -10,7 +10,7 @@ from covid19.data import preproc
 from covid19.data import generate
 
 
-def create_viz_growth_simulation(charts_path: str):
+def create_viz_growth_simulation(charts_path: str) -> None:
     start_date = datetime.date(2020, 1, 1)
     end_date = datetime.date(2020, 1, 15)
 
@@ -80,14 +80,9 @@ def create_viz_growth_simulation(charts_path: str):
     )
 
 
-def create_viz_line_chart_with_ruler_as_selector(df: pd.DataFrame):
-    # Create a selection that chooses the nearest point & selects based on x-value
-    nearest_selection = alt.selection(
-        type="single", nearest=True, on="mouseover", fields=["x"], empty="none"
-    )
-
-
-def create_viz_world_confirmed_and_rate(charts_path: str, dfs: Dict[str, pd.DataFrame]):
+def create_viz_world_confirmed_and_rate(
+    charts_path: str, dfs: Dict[str, pd.DataFrame]
+) -> None:
     df_world_agg_confirmed = dfs["world-agg-confirmed"]
     df_world_agg_growth_rate = dfs["world-agg-growth-rate"]
 
@@ -126,15 +121,23 @@ def create_viz_world_confirmed_and_rate(charts_path: str, dfs: Dict[str, pd.Data
 
     alt.vconcat(
         add_ruler_as_selector_on_single_line_chart(
-            world_agg_confirmed_chart, df_world_agg_confirmed, "date", "count"
+            world_agg_confirmed_chart,
+            df_world_agg_confirmed,
+            x_field="date",
+            y_field="count",
         ),
         add_ruler_as_selector_on_single_line_chart(
-            world_agg_growth_rate_chart, df_world_agg_growth_rate, "date", "growth_rate"
+            world_agg_growth_rate_chart,
+            df_world_agg_growth_rate,
+            x_field="date",
+            y_field="growth_rate",
         ),
     ).save(viz_path, embed_options={"renderer": "svg"})
 
 
-def add_ruler_as_selector_on_single_line_chart(chart, df, x_field, y_field):
+def add_ruler_as_selector_on_single_line_chart(
+    chart: Any, df: pd.DataFrame, x_field: str, y_field: str
+) -> Any:
     # Create a selection that chooses the nearest point & selects based on x-value
     nearest = alt.selection(
         type="single", nearest=True, on="mouseover", fields=["date"], empty="none",
@@ -145,19 +148,19 @@ def add_ruler_as_selector_on_single_line_chart(chart, df, x_field, y_field):
     selectors = (
         alt.Chart(df)
         .mark_point()
-        .encode(x=x_field, opacity=alt.value(0),)
+        .encode(x=x_field, opacity=alt.value(0), tooltip=alt.Tooltip(y_field),)
         .add_selection(nearest)
     )
 
     # Draw points on the line, and highlight based on selection
     points = chart.mark_point().encode(
-        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0)),
     )
 
-    # Draw text labels near the points, and highlight based on selection
-    text = chart.mark_text(align="left", dx=5, dy=-5).encode(
-        text=alt.condition(nearest, y_field, alt.value(" "))
-    )
+    # # Draw text labels near the points, and highlight based on selection
+    # text = chart.mark_text(align="left", dx=5, dy=-5).encode(
+    #     text=alt.condition(nearest, y_field, alt.value(" "))
+    # )
 
     # Draw a rule at the location of the selection
     rules = (
@@ -168,12 +171,12 @@ def add_ruler_as_selector_on_single_line_chart(chart, df, x_field, y_field):
     )
 
     # Put the five layers into a chart and bind the data
-    return alt.layer(chart, selectors, points, rules, text)
+    return alt.layer(chart, selectors, points, rules)
 
 
 def create_viz_region_confirmed_and_rate(
     charts_path: str, dfs: Dict[str, pd.DataFrame]
-):
+) -> None:
     df_region_agg_confirmed = dfs["region-agg-confirmed"]
     df_region_agg_growth_rate = dfs["region-agg-growth-rate"]
 
@@ -224,7 +227,7 @@ def create_viz_region_confirmed_and_rate(
 
 def add_ruler_to_multi_line_chart(
     df: pd.DataFrame, x: (str, str), category: (str, str), line: Any,
-):
+) -> Any:
     x_field, x_title = x
     category_field, category_title = category
 
@@ -264,7 +267,7 @@ def add_ruler_to_multi_line_chart(
     return alt.layer(line, selectors, points, rules, text)
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
         "--data-path", required=True, help="Dir with processed data"

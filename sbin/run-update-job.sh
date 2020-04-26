8a8b8af494ade51f45f212267174132e4ef0a508
guilherme <guilherme.dinisjunior@gmail.com>
2020-04-26 21:45:20 +0200

    Adds update job script

--- sbin/run-update-job.sh
+++ sbin/run-update-job.sh
@@ -6,8 +6,15 @@ function run(){
     DIR=$(dirname $0)
     BASEDIR=${DIR}/..
 
-    export PYTHONPATH=
-    run-data-pull-jhu.sh
+    export PYTHONPATH=$BASEDIR/py
+    sh $BASEDIR/sbin/run-data-pull-jhu.sh
+    python -m covid19.data.preproc \
+      --input-path=/Users/guilherme/code/covid19/data/jhu/csse_covid_19_data/csse_covid_19_daily_reports/ \
+      --output-path=/Users/guilherme/code/covid19/data/jhu/processed
+    python -m covid19.viz.partials \
+      --plots-path=/Users/guilherme/code/covid19/data/jhu/processed/charts \
+      --output-path=/Users/guilherme/code/covid19/docs/_partials \
+      --files region-agg-chart.html world-agg-chart.html
 
 }
 

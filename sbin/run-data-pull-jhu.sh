42dfb00cfee4e1331307f14381208bc7694d19f9
guilherme <guilherme.dinisjunior@gmail.com>
2020-05-03 17:58:46 +0200

    Fixes update script

--- sbin/run-data-pull-jhu.sh
+++ sbin/run-data-pull-jhu.sh
@@ -13,7 +13,6 @@ function run(){
     else
       git clone git@github.com:CSSEGISandData/COVID-19.git $BASEDIR/data/jhu
     fi;
-    head -n 10 $BASEDIR/data/jhu/archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv
 
 }
 
--- sbin/run-update-job.sh
+++ sbin/run-update-job.sh
@@ -7,10 +7,22 @@ function run(){
     BASEDIR=${DIR}/..
 
     export PYTHONPATH=$BASEDIR/py
-    sh $BASEDIR/sbin/run-data-pull-jhu.sh
+
+    if [ -d $BASEDIR/data/jhu/.git ]; then
+      cd $BASEDIR/data/jhu
+      git pull
+      cd ../../
+    else
+      git clone git@github.com:CSSEGISandData/COVID-19.git $BASEDIR/data/jhu
+    fi;
+
     python -m covid19.data.preproc \
       --input-path=/Users/guilherme/code/covid19/data/jhu/csse_covid_19_data/csse_covid_19_daily_reports/ \
       --output-path=/Users/guilherme/code/covid19/data/jhu/processed
+
+    python -m covid19.viz.createviz \
+      --data-path=/Users/guilherme/code/covid19/data/jhu/processed
+
     python -m covid19.viz.partials \
       --plots-path=/Users/guilherme/code/covid19/data/jhu/processed/charts \
       --output-path=/Users/guilherme/code/covid19/docs/_partials \
